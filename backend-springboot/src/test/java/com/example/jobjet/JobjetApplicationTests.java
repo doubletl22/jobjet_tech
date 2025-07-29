0084848528d7146ece1d50c4a928c419188c80c4
doubletl22 <kiyoshineko41@gmail.com>
2025-07-29 18:52:36 +0700

    tạo backend

---
+++ backend-springboot/src/test/java/com/example/jobjet/JobjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
